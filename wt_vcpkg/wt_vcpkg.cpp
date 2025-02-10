a96c953575d2b443d01a4eb15873c6bca0c1b785
Mats Büscher <mats.buescher@gmx.de>
2025-02-10 02:23:05 +0100

    Add project files.

---
+++ wt_vcpkg/wt_vcpkg.cpp
@@ -0,0 +1,14 @@
+﻿// wt_vcpkg.cpp : Defines the entry point for the application.
+//
+
+#include <Wt/WApplication.h>
+#include <Wt/Dbo/backend/Postgres.h>
+
+using namespace std;
+
+int main()
+{
+	auto connection = std::make_unique<Wt::Dbo::backend::Postgres>("SQL_CONNECTION_STRING");
+	
+	printf("connection: %p\n", connection.get());
+}
